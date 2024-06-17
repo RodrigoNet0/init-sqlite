0194d172a300ffb54cfcacce731f525040677927
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-06-17 19:17:04 +0000

    feat: create bank

---
+++ app.js
@@ -0,0 +1,13 @@
+import sqlite3 from 'sqlite3';
+
+import { open } from 'sqlite';
+ 
+async function createTable(name, lastname){
+    const db = await open({
+        filename: './banco.db',
+        driver: sqlite3.Database,
+    });
+    db.run(`CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, lastname TEXT)`)
+}
+
+createTable();
